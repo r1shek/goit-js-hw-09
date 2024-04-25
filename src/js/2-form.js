b394b9b8c4c081a8192401d9ce0234626ab8d570
r1shek <rostic523@gmail.com>
2024-04-25 18:28:10 +0200

    1

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+function saveFormData() {
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function fillUpForm() {
+  const savedData = localStorage.getItem(localStorageKey);
+
+  if (savedData) {
+    const savedFormData = JSON.parse(savedData);
+    form.elements.email.value = savedFormData.email;
+    form.elements.message.value = savedFormData.message;
+  }
+}
+
+fillUpForm();
+
+form.addEventListener('input', () => {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  saveFormData();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log('Email:', formData.email);
+    console.log('Message:', formData.message);
+    localStorage.removeItem(localStorageKey);
+    formData.email = '';
+    formData.message = '';
+    form.reset();
+  }
+});
